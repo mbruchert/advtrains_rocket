1b6729a1c2a9a8a435eecf1d568532b513b63b38
Blockhead <jbis1337@hotmail.com>
2021-10-25 12:07:21 +1100

    Remove default gui_bg as it is not used/needed

--- init.lua
+++ init.lua
@@ -141,15 +141,13 @@ minetest.register_craft({
 
 local barrel_formspec =
 	"size[8,9]" ..
-	default.gui_bg ..
-	default.gui_bg_img ..
-	default.gui_slots ..
 	"list[current_name;main;0,0.3;8,4;]" ..
 	"list[current_player;main;0,4.85;8,1;]" ..
 	"list[current_player;main;0,6.08;8,3;8]" ..
 	"listring[current_name;main]" ..
-	"listring[current_player;main]" ..
-	default.get_hotbar_bg(0,4.85)
+	"listring[current_player;main]"
+	--..
+	--default.get_hotbar_bg(0,4.85)
 
 minetest.register_node("advtrains_train_rocket:barrel", {
 	description = "barrel",
